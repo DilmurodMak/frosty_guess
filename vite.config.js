0bce88fb53cd24081b93224259ad0188dd21d4bb
Dilik <1989deny@gmail.com>
2025-11-18 22:37:19 -0500

    fix issues

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/snowman/' : '/',
+  base: mode === 'production' ? '/frosty_guess/' : '/',
   test: {
     globals: true,
     environment: 'jsdom',
